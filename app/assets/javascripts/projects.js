3a8eb9f6a455ed7495ff4c68cfccf9a1934bd3ea
mlefurge <matthew.lefurge@gmail.com>
2015-12-11 22:33:38 -0500

    Add basic flip functionality

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
 
-  $("#card").flip({
-    axis: 'x',
-    trigger: 'hover'
-  });
+    $(".flippable").click(function() {
+    var front = $(this).children(".front");
+    var back = $(this).children(".back");
+    var toHide = front.is(':visible') ? front : back;
+    var toShow = back.is(':visible') ? front : back;
 
+    toHide.removeClass('flip in').addClass('flip out').hide();
+    toShow.removeClass('flip out').addClass('flip in').show();
+
+  });
 
 })
