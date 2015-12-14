2c3e9c72f43cc11da6a67d9d92ac608b754ee7f7
mlefurge <matthew.lefurge@gmail.com>
2015-12-14 10:11:31 -0500

    Comment out contact me button jquery

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -1,17 +1,17 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-  $('#contact_me').on('click', function(){
-    $('.generic_info').slideToggle();
-    $('#contact_me').fadeToggle();
-    $('.contact_form_container').slideToggle();
-  });
+//   $('#contact_me').on('click', function(){
+//     $('.generic_info').slideToggle();
+//     $('#contact_me').fadeToggle();
+//     $('.contact_form_container').slideToggle();
+//   });
 
-  $('#cancel').on('click', function(event){
-    event.preventDefault();
-    $('.contact_form_container').slideToggle();
-    $('#contact_me').fadeToggle();
-    $('.generic_info').slideToggle();
-  })
+//   $('#cancel').on('click', function(event){
+//     event.preventDefault();
+//     $('.contact_form_container').slideToggle();
+//     $('#contact_me').fadeToggle();
+//     $('.generic_info').slideToggle();
+//   })
 
 
-})
+// })
